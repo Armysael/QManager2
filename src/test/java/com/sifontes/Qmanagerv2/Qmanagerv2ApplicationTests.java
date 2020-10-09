7cd64e36ca8a9a4c7f622fe49a949f28f00f0047
EDUARDO SIFONTES ALVAREZ <44906125+Armysael@users.noreply.github.com>
2020-10-09 09:04:16 -0300

    Initial setup Project

---
+++ src/test/java/com/sifontes/Qmanagerv2/Qmanagerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sifontes.Qmanagerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qmanagerv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
